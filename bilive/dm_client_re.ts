7abe9c6a18969011e5505e44c1c9273a097095e9
lzghzr <lzggzr@gmail.com>
2020-04-19 00:23:18 +0800

    更改重连逻辑

--- bilive/dm_client_re.ts
+++ bilive/dm_client_re.ts
@@ -39,7 +39,7 @@ class DMclientRE extends DMclient {
       }
       else {
         this.reConnectTime++
-        this.Connect({ server: this._server, port: this.port, key: this.key })
+        this.Connect()
       }
     }, 3 * 1000)
   }
